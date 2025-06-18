b28557acb34d69d5a8a7b3f9d7559e103f5fd820
AlexOFF <vladimiroff.all@yandex.ru>
2025-06-18 05:15:10 +0300

    HW3

--- askme_vladimirov/app/urls.py
+++ askme_vladimirov/app/urls.py
@@ -20,13 +20,12 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
-    path('hot/', views.hot, name="hot"),
-    path('login/', views.login, name="login"),
-    path('ask/', views.ask, name="ask"),
-    path('question/<int:question_id>', views.question, name="question"),
-    path('settings/', views.settings, name="settings"),
-    path('signup/', views.signup, name="signup"),
-    path('tag/<int:tag_id>', views.tag, name="tag"),
+    path('', views.index, name='index'),
+    path('hot/', views.hot, name='hot'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
+    path('tag/<str:tag_name>/', views.tag, name='tag'),
+    path('login/', views.login, name='login'),
+    path('register/', views.signup, name='register'),
+    path('settings/', views.settings, name='settings'),
 ]
--- askme_vladimirov/app/views.py
+++ askme_vladimirov/app/views.py
@@ -1,59 +1,87 @@
 import copy
 
-from django.core.paginator import Paginator
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from app import models
 
-QUESTIONS = [
-    {
-        'title': f'Title {i}',
-        'id': i,
-        'text': f'This is text for question #{i}',
-        'img_path': '/img/dreamcar.jpg'
-    } for i in range(30)
-]
-
-ANSWERS = [
-    {
-        'id': i,
-        'text': f'Text for answer #{i}',
-        'img_path': '/img/dreamcar.jpg'
-    } for i in range(30)
-]
-
-TAGS = [
-    {
-        'id': i,
-        'name': f'Tag {i}'
-    } for i in range(5)
-]
+def paginate(object_list, request, per_page=5):
+    paginator = Paginator(object_list, per_page)
+    page = request.GET.get('page', 1)
+    try:
+        paginator.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        raise Http404("Page not found")
+    return paginator.page(page)
 
 def index(request):
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(QUESTIONS, 5)
-    page = paginator.page(page_num)
-    return render(request, 'index.html', context={'questions': page.object_list, 'page_obj': page, 'tags': TAGS})
+    questions = paginate(models.Question.objects.get_new_questions(), request)
+    context = {
+        'questions': questions,
+        'page_obj': questions,
+        'tags': models.Tag.objects.get_popular_tags(),
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+
+    return render(request, 'index.html', context=context)
+
 def hot(request):
-    q = list(reversed(copy.deepcopy(QUESTIONS)))
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(q, 5)
-    page = paginator.page(page_num)
-    return render(request, 'hot.html', context={'questions': page.object_list, 'page_obj': page, 'tags': TAGS})
+    questions = paginate(models.Question.objects.get_hot_questions(), request)
+    context = {
+        'questions': questions,
+        'page_obj': questions,
+        'tags': models.Tag.objects.get_popular_tags(),
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+    return render(request, 'hot.html', context=context)
+
 def signup(request):
-    return render(request, 'signup.html', context={'tags': TAGS})
+    context = {
+        'tags': models.Tag.objects.get_popular_tags(),
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+    return render(request, 'signup.html', context=context)
+
 def login(request):
-    return render(request, 'login.html', context={'tags': TAGS})
+    context = {
+        'tags': models.Tag.objects.get_popular_tags(),
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+    return render(request, 'login.html', context=context)
+
 def question(request, question_id):
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(ANSWERS, 5)
-    page = paginator.page(page_num)
-    return render(request, 'question.html', context={'question': QUESTIONS[question_id], 'page_obj': page, 'tags': TAGS, 'answers': page.object_list})
+    answers = paginate(models.Answer.objects.get_answers(question_id), request)
+    question = get_object_or_404(models.Question, id=question_id)
+    context = {
+        'question': question,
+        'answers': answers,
+        'tags': models.Tag.objects.get_popular_tags(),
+        'page_obj': answers,
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+    return render(request, 'question.html', context=context)
+
 def ask(request):
-    return render(request, 'ask.html', context={'tags': TAGS})
+    context = {
+        'tags': models.Tag.objects.get_popular_tags(),
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+    return render(request, 'ask.html', context=context)
+
 def settings(request):
-    return render(request, 'settings.html', context={'tags': TAGS})
-def tag(request, tag_id):
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(QUESTIONS, 5)
-    page = paginator.page(page_num)
-    return render(request, 'tag.html', context={'questions': page.object_list, 'page_obj': page, 'tags': TAGS, 'tag': TAGS[tag_id]})
+    context = {
+        'tags': models.Tag.objects.get_popular_tags(),
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+    return render(request, 'settings.html', context=context)
+
+def tag(request, tag_name):
+    questions = paginate(models.Question.objects.get_questions_by_tag(tag_name), request)
+    context = {
+        'tag_name': tag_name,
+        'questions': questions,
+        'page_obj': questions,
+        'tags': models.Tag.objects.get_popular_tags(),
+        'profiles': models.Profile.objects.get_popular_profiles(),
+    }
+    return render(request, 'tag.html', context=context)
